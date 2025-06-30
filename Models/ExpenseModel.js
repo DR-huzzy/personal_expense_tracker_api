dc3844fd7337e1bef2554d87d7bdd28fe655d1f7
DR-huzzy <drhuzzy1234@gmail.com>
2025-06-30 16:01:13 +0000

    creating the expense model

---
+++ Models/ExpenseModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+
+    category: {
+        type: String,
+        required: true,
+        enum: [Groceries, Leisure, Electronics, Utilities, Clothing, Health, Others]
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    description: {
+        type: String,
+        trim: true
+        // Reference to the User model
+    },
+
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+
+const Expense = new mongoose.model('Expense',expenseSchema);
+
+module.exports = Expense;
+
